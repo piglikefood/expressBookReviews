2267d69cc8e4a3e120fc335fa4e76d0bc38957a2
piglikefood <154314073+piglikefood@users.noreply.github.com>
2024-01-03 05:29:48 +0000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,12 +11,45 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+
+    if(username != "" & password != ""){
+        let userswithsamename = users.filter((user)=>{
+            return user.username === username
+        });
+        if(userswithsamename.length > 0){
+            // check password
+
+            return true;
+        } else {
+            users.push({username : username, password: password})
+            return false;
+        }       
+    }else{
+        return false;
+    }
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(username != "" & password != ""){
+        if(authenticatedUser(username, password)){
+            let accessToken = jwt.sign({
+                data: password
+              }, 'access', { expiresIn: 60 * 60 });
+          
+              req.session.authorization = {
+                accessToken,username
+              }
+              return res.status(200).send("User successfully logged in");
+        }    
+   }else{
+       return res.status(300).json({message: "Username and password cannot be empty."});
+   }
 });
 
 // Add a book review
