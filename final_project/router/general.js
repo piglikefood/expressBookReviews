7db5a8f1bb975fe37820ebeb19aa0bd05c201ce3
piglikefood <154314073+piglikefood@users.noreply.github.com>
2024-01-03 05:30:09 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,19 +4,19 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
     let username = req.body.username;
     let password = req.body.password;
 
-    if(username && password){
-        let userswithsamename = users.filter((username)=>{
-            return users.username === username
+    if(username != "" & password != ""){
+        let userswithsamename = users.filter((user)=>{
+            return user.username === username
         });
         if(userswithsamename.length > 0){
             return res.status(300).json({message: "Username already exist."});
         } else {
+            users.push({username : username, password: password})
             return res.send(`Username ${username} registered successfully.`)
         }       
     }else{
