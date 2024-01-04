e1d2c8e19a03616d30b11a4bbcda5593975070e1
piglikefood <154314073+piglikefood@users.noreply.github.com>
2024-01-04 14:35:42 +0000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [{username: "test", password: "123"}];
+let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
